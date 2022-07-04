ed8322fa3bc98dabb86792456abdc70ea59c5f54
zhangtaimin <616239001@qq.com>
2022-07-04 18:41:33 +0800

    初始化

---
+++ src/components/UsePinia.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>欢迎使用新时代前端框架</h2>
+  <div>{{ userStore.name }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useUserStore } from '@/store/user'
+
+export default defineComponent({
+  name: 'UsePinia',
+  setup() {
+    const userStore = useUserStore()
+
+    return {
+      userStore,
+    }
+  },
+})
+</script>
